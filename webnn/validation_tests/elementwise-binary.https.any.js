82b8bb7eb79bcd6650fed0549bead0b9213ee618
mei1127 <shanxing.mei@intel.com>
2024-05-14 20:43:05 -0700

    webnn: Migrate ElementWiseBinary validation tests to WPTs
    This CL adds WPT tests for ElementWiseBinary and removes the unit tests
    `MLGraphBuilderElementWiseBinaryTest.TestElementWiseBinary` ,
    `MLGraphTestMojo.ElementWiseBinaryTest`,
    `MLGraphTestMojo.ElementWiseBinaryLogicalTest` and `MLGraphTest,
    ElementWiseBinaryTest`.
    
    Bug: 327337526, 328026885
    Change-Id: I21b96ea03dc5dc070173151f53ca7d5b63a73281

--- webnn/validation_tests/elementwise-binary.https.any.js
+++ webnn/validation_tests/elementwise-binary.https.any.js
@@ -14,8 +14,70 @@ const kElementwiseBinaryOperators = [
   'pow',
 ];
 
+
+const tests = [
+  {
+    name:
+        '[binary] Test with two input dimensions - [8, 1, 6, 1] and [7, 1, 5].\
+     Both the a and b dimensions have axes with length one that are\
+     expanded to a larger size during the broadcast operation.\
+     a_dimensions     (4d) 8 * 1 * 6 * 1\
+     b_dimensions     (3d)     7 * 1 * 5\
+     output_dimenions (4d) 8 * 7 * 6 * 5',
+    a: {dataType: 'float32', dimensions: [8, 1, 6, 1]},
+    b: {dataType: 'float32', dimensions: [7, 1, 5]},
+    output: {dataType: 'float32', dimensions: [8, 7, 6, 5]}
+  },
+  {
+    name: '[binary] Test with two input dimensions - [4, 2, 1] and [4].\
+    a_dimensions     (3d) 4 * 2 * 1\
+    b_dimensions     (1d)         4\
+    output_dimenions (3d) 4 * 2 * 4',
+    a: {dataType: 'float32', dimensions: [4, 2, 1]},
+    b: {dataType: 'float32', dimensions: [4]},
+    output: {dataType: 'float32', dimensions: [4, 2, 4]}
+  },
+  {
+    name:
+        '[binary] Testing scalar broadcasting by building with two input dimensions [4,2,4] and [].',
+    a: {dataType: 'float32', dimensions: [4, 2, 4]},
+    b: {dataType: 'float32', dimensions: []},
+    output: {dataType: 'float32', dimensions: [4, 2, 4]}
+  },
+  {
+    name: '[binary] Throw if the input shapes are not broadcastable.',
+    a: {dataType: 'float32', dimensions: [4, 2]},
+    b: {dataType: 'float32', dimensions: [4]},
+  },
+  {
+    name: '[binary] Throw if the input types don\'t match.',
+    a: {dataType: 'float32', dimensions: [4, 2]},
+    b: {dataType: 'int32', dimensions: [1]},
+  },
+];
+
+function runElementWiseBinaryTests(operatorName, tests) {
+  tests.forEach(test => {
+    promise_test(async t => {
+      const a = builder.input(
+          'a', {dataType: test.a.dataType, dimensions: test.a.dimensions});
+      const b = builder.input(
+          'b', {dataType: test.b.dataType, dimensions: test.b.dimensions});
+
+      if (test.output) {
+        const output = builder[operatorName](a, b);
+        assert_equals(output.dataType(), test.output.dataType);
+        assert_array_equals(output.shape(), test.output.dimensions);
+      } else {
+        assert_throws_js(TypeError, () => builder[operatorName](a, b));
+      }
+    }, test.name.replace('[binary]', `[${operatorName}]`));
+  });
+}
+
 kElementwiseBinaryOperators.forEach((operatorName) => {
   validateTwoInputsOfSameDataType(operatorName);
   validateTwoInputsBroadcastable(operatorName);
   validateTwoInputsFromMultipleBuilders(operatorName);
+  runElementWiseBinaryTests(operatorName, tests);
 });
